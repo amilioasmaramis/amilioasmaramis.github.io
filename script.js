3aa1228975dd7b70dadf2761f632537d86630cd8
amilioasmaramis <amilio.asmaramis@gmail.com>
2020-12-18 13:05:35 +0700

    Amilio

--- script.js
+++ script.js
@@ -8,30 +8,69 @@ function playSong(hero) {
     spoilerGambar.src = "image/" + hero + ".jpg";
     spoilerVideo.src = "video/" + hero + ".mp4";
     spoilerVideo.play();
+    spoilerRandom.src = "image/1.jpg";
 }
 
 let spoilerRandom = document.getElementById('spoilerRandom');
 
 function playRandom() {
     var myImages1 = new Array();
-    myImages1[0] = "image/1.jpg";
-    myImages1[1] = "image/Invoker.jpg";
-    myImages1[2] = "image/Wd.jpg";
-    myImages1[3] = "image/Wr.jpg";
-    myImages1[4] = "image/BountyHunter.jpg";
-    myImages1[5] = "image/Bs.jpg";
-    myImages1[6] = "image/Earthseeker.jpg";
-    myImages1[7] = "image/Juggernaut.jpg";
-    myImages1[8] = "image/Kunka.jpg";
-    myImages1[9] = "image/Pudge.jpg";
-    myImages1[10] = "image/Techis.jpg";
-    myImages1[11] = "image/Void.jpg";
-    var rnd = Math.floor(Math.random() * myImages1.length);
-    if (rnd == 0) {
-            rnd = 1;
-    } 
+    myImages1[0] = "image/Kunka.jpg";
+    myImages1[1] = "image/Earthseeker.jpg";
+    myImages1[2] = "image/BountyHunter.jpg";
+    myImages1[3] = "image/Juggernaut.jpg";
+    myImages1[4] = "image/Invoker.jpg";
+    myImages1[5] = "image/Wd.jpg";
+
+    var video = new Array();
+    video[0] = "Kunka";
+    video[1] = "Earthseeker";
+    video[2] = "BountyHunter";
+    video[3] = "Juggernaut";
+    video[4] = "Invoker";
+    video[5] = "Wd";
+
+    var rnd = Math.floor(Math.random() * myImages1.length); 
+    // spoilerVideo.src = "video/" + hero + ".mp4";
     spoilerRandom.src = myImages1[rnd];
+    switch (rnd) {
+        case 0:
+            spoilerRandom.src = myImages1[rnd];
+            spoilerVideo.src = "video/" + video[rnd] + ".mp4";
+            spoilerVideo.play();
+            spoilerGambar.src = myImages1[rnd];
+            break;
+        case 1:
+            spoilerRandom.src = myImages1[rnd];
+            spoilerVideo.src = "video/" + video[rnd] + ".mp4";
+            spoilerVideo.play();
+            spoilerGambar.src = myImages1[rnd];
+            break;
+        case 2:
+            spoilerRandom.src = myImages1[rnd];
+            spoilerVideo.src = "video/" + video[rnd] + ".mp4";
+            spoilerVideo.play();
+            spoilerGambar.src = myImages1[rnd];
+            break;
+        case 3:
+            spoilerRandom.src = myImages1[rnd];
+            spoilerVideo.src = "video/" + video[rnd] + ".mp4";
+            spoilerVideo.play();
+            spoilerGambar.src = myImages1[rnd];
+            break;
+        case 4:
+            spoilerRandom.src = myImages1[rnd];
+            spoilerVideo.src = "video/" + video[rnd] + ".mp4";
+            spoilerVideo.play();
+            spoilerGambar.src = myImages1[rnd];
+            break;
+        case 5:
+            spoilerRandom.src = myImages1[rnd];
+            spoilerVideo.src = "video/" + video[rnd] + ".mp4";
+            spoilerVideo.play();
+            spoilerGambar.src = myImages1[rnd];
+            break;
+    }
+    
 }
-1920
-1080
 
